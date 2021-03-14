019be6b545d87761a090c139c1b49e741cdb3284
Ken Chau <ken@gizzar.com>
2021-03-13 22:44:07 -0800

    updates

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    cssnano: {
-      preset: 'default',
-    }
+    // cssnano: {
+    //   preset: 'default',
+    // }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,24 @@ module.exports = {
     enabled: true,
     content: ["./src/blocks/*/*/*.js"],
     options: {
-      safelist: ["dark"],
+      safelist: ["dark", /-theme-/],
     },
   },
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        100: 'var(--theme-100)',
+        200: 'var(--theme-200)',
+        300: 'var(--theme-300)',
+        400: 'var(--theme-400)',
+        500: 'var(--theme-500)',
+        600: 'var(--theme-600)',
+        700: 'var(--theme-700)',
+        800: 'var(--theme-800)',
+        900: 'var(--theme-900)'
+      }
+    },
   },
   variants: {},
   plugins: [],
