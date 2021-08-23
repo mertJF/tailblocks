7c465005bd9fbc31d679c937aeedd5ea63241100
Manitej66 <pratamaniteja66@gmail.com>
2021-08-23 13:11:51 +0530

    added 404 component

---
+++ src/icons/Notfound/a.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+function NotfoundA() {
+  return (
+    <svg viewBox="0 0 266 150" fill="none">
+      <path fill="var(--solid)" d="M0 0h266v150H0z" />
+
+      <rect x={95} y={58} width={76} height={4} rx={2} fill="var(--base-500)" />
+      <rect
+        x={123}
+        y={94}
+        width={20}
+        height={2}
+        rx={1}
+        fill="var(--main-500)"
+      />
+      <rect x={89} y={66} width={88} height={4} rx={2} fill="var(--base-500)" />
+      <rect x={92} y={74} width={83} height={4} rx={2} fill="var(--base-500)" />
+      <rect
+        x={103}
+        y={82}
+        width={60}
+        height={4}
+        rx={2}
+        fill="var(--base-500)"
+      />
+      <rect
+        x={113}
+        y={104}
+        width={40}
+        height={4}
+        rx={2}
+        fill="var(--solid-900)"
+      />
+      <rect
+        x={106}
+        y={112}
+        width={54}
+        height={4}
+        rx={2}
+        fill="var(--base-400)"
+      />
+    </svg>
+  );
+}
+
+export default NotfoundA;
