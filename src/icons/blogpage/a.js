abb4c12545df38fc2bd8d0dfb2f69eb84b19d52f
Mohit Maroliya <mmaroliya@gmail.com>
2022-07-26 15:05:37 +0530

    Adding the blogpageA icon

--- src/icons/blogpage/a.js
+++ src/icons/blogpage/a.js
@@ -1,38 +1,87 @@
-import React from "react"
+import React from "react";
 
 function BlogPageA() {
   return (
     <svg viewBox="0 0 266 150" fill="none">
       <path fill="var(--solid)" d="M0 0h266v150H0z" />
-      <path
-        d="M79 88a2 2 0 012-2h25a2 2 0 012 2v6a2 2 0 01-2 2H81a2 2 0 01-2-2v-6z"
-        fill="var(--main-500)"
-      />
+
       <rect
         x={20}
-        y={86}
-        width={55}
-        height={10}
-        rx={2}
-        fill="var(--base-400)"
+        y={28}
+        width={125}
+        height={5}
+        rx={2.5}
+        fill="var(--solid-900)"
       />
+
+      <circle cx={27.5} cy={47.5} r={7.5} fill="var(--base-300)" />
+      <rect
+        x={39}
+        y={49}
+        width={32}
+        height={3}
+        rx={1.5}
+        fill="var(--base-500)"
+      />
+      <rect
+        x={39}
+        y={43}
+        width={35}
+        height={3}
+        rx={1.5}
+        fill="var(--solid-900)"
+      />
+
       <rect
         x={20}
         y={64}
-        width={104}
-        height={4}
+        width={130}
+        height={3}
         rx={2}
         fill="var(--base-500)"
       />
       <rect
         x={20}
-        y={53}
-        width={72}
-        height={5}
-        rx={2.5}
-        fill="var(--solid-900)"
+        y={70}
+        width={130}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
       />
-      <rect x={20} y={72} width={97} height={4} rx={2} fill="var(--base-500)" />
+      <rect
+        x={20}
+        y={76}
+        width={130}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
+      />
+
+      <rect
+        x={20}
+        y={86}
+        width={130}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
+      />
+      <rect
+        x={20}
+        y={92}
+        width={130}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
+      />
+
+      <rect
+        x={181}
+        y={98}
+        width={20}
+        height={3}
+        rx={2}
+        fill="var(--main-500)"
+      />
       <path
         d="M176.778 92h26.444A3.778 3.778 0 00207 88.222V61.778A3.778 3.778 0 00203.222 58h-26.444A3.778 3.778 0 00173 61.778v26.444A3.778 3.778 0 00176.778 92zm0 0l20.778-20.778L207 80.667m-20.778-12.278a2.833 2.833 0 11-5.666 0 2.833 2.833 0 015.666 0z"
         stroke="var(--base-500)"
@@ -40,8 +89,41 @@ function BlogPageA() {
         strokeLinecap="round"
         strokeLinejoin="round"
       />
+
+      <rect
+        x={20}
+        y={110}
+        width={190}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
+      />
+      <rect
+        x={20}
+        y={116}
+        width={190}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
+      />
+
+      <circle cx={25} cy={126} r={3} fill="var(--base-500)" />
+      <circle cx={33} cy={126} r={3} fill="var(--base-500)" />
+
+      <circle cx={97} cy={133} r={3} fill="var(--main-500)" />
+      <circle cx={106} cy={133} r={3} fill="var(--main-500)" />
+      <circle cx={116} cy={133} r={3} fill="var(--main-500)" />
+      <circle cx={126} cy={133} r={3} fill="var(--main-500)" />
+      <rect
+        x={95}
+        y={139}
+        width={35}
+        height={3}
+        rx={2}
+        fill="var(--base-500)"
+      />
     </svg>
-  )
+  );
 }
 
 export default BlogPageA;
